812f29d6b9e4bd6a89c8b3dafa8b5b4c430d5d5f
Shikha Bhatnagar <shikhabhatnagar210905@gmail.com>
2026-03-17 22:44:26 +0530

    Time: 658 ms (13.80%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,18 +1,21 @@
 # Write your MySQL query statement below
-SELECT t.request_at as Day,
-ROUND(
-    SUM(CASE
-          WHEN t.status!='completed' THEN 1
-          ELSE 0
-          END)/COUNT(*),
-     2
-)AS "Cancellation Rate"
+SELECT
+    t.request_at AS Day,
+    ROUND(
+        SUM(
+            CASE 
+                WHEN t.status != 'completed' THEN 1 
+                ELSE 0 
+            END
+        ) / COUNT(*),
+        2
+    ) AS "Cancellation Rate"
 FROM Trips t
 JOIN Users c
-     ON t.client_id = c.users_id
+    ON t.client_id = c.users_id
 JOIN Users d
-     On t.driver_id= d.users_id
+    ON t.driver_id = d.users_id
 WHERE c.banned = 'No'
-   And d.banned='No'
-   and t.request_at BETWEEN '2013-10-01' And '2013-10-03'
-GROUP BY t.request_at
+  AND d.banned = 'No'
+  AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at;
